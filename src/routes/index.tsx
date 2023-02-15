1e063ccefe37869e62676ceb09aad11f33d0ced0
Charl Kruger <nit3watch@gmail.com>
2023-02-15 12:29:18 +0200

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,30 @@
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import { Link } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  return (
+    <div>
+
+      <Link href="/123/">
+        Link to 123
+      </Link>
+
+        <br/>
+
+      <a href="/123">
+        a tag to 123
+      </a>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+};
